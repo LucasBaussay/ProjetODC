154031213f4d21863cd632ab683a32158b86f832
Lucas Baussay <lucas.baussay.lb@gmail.com>
2020-11-10 16:21:04 +0100

    Add Show for struct

--- src/structs.jl
+++ src/structs.jl
@@ -3,12 +3,16 @@
 structs.jl déclare les nouveaux types utilisés dans ce projet.
 """
 
+import Base.show
+
 # objet Station modélisant une station de navette
 struct Station
     name::String
     latitude::Float64
     longitude::Float64
 end
+
+Base.show(io::IO, x::Station) = print(io, x.name)
 
 # objet Subdivision modélisant un lotissement urbain
 struct Subdivision
@@ -18,3 +22,5 @@ struct Subdivision
     density::Int
     nearStations::Array{Station,1}
 end
+
+Base.show(io::IO, x::Subdivision) = println(io, x.name)

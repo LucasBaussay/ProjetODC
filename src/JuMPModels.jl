929e1a39999182a140aaeda0486af05e99511009
jnicolasthouvenin <jules.nicolasthouvenin@gmail.com>
2020-11-10 17:24:21 +0100

    using Jump glpk

--- src/JuMPModels.jl
+++ src/JuMPModels.jl
@@ -11,6 +11,6 @@ function MCP_model(stations::Array{Station,1},townsites::Array,p::Int,h::Array{F
     @variable(m, s[1:nbTownsites], Bin) # si = 1 si la demande du lotissement i est satisfaite, 0 sinon
     @constraint(m, activation[1:nbTownsites], sum(x[station] - s[townsite] for station=1:nbStations) >= 0)
     @constraint(m, limit, sum(x[station] for station=1:nbStations) == p)
-    @objective(ip, Max, dot(h,s))
+    @objective(m, Max, dot(h,s))
     return m, x
 end

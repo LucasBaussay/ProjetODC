ca7a51de4ebd3d48101ac17b105d71a641446c71
jnicolasthouvenin <40352310+jnicolasthouvenin@users.noreply.github.com>
2020-11-19 16:45:04 +0100

    suppression de mon model

--- src/JuMPModels.jl
+++ src/JuMPModels.jl
@@ -3,35 +3,6 @@
 JuMPModels.jl gère les modèles JuMP et leur résolution
 """
 
-function MCP_model(stations::Array{Station,1},townsites::Array,p::Int, verbose::Bool = true)
-    m = Model(GLPK.Optimizer)
-    nbStations = length(stations)
-    nbTownsites = length(townsites)
-    h::Vector{Float64} = getDensity.(townsites)
-
-    @variable(m, x[1:nbStations], Bin) # xi = 1 si la station i est construite, 0 sinon
-    @variable(m, s[1:nbTownsites], Bin) # si = 1 si la demande du lotissement i est satisfaite, 0 sinon
-    
-    @constraint(m, activation[ townsite = 1:nbTownsites], sum(x[station] - s[townsite] for station=1:nbStations) >= 0) #TODO : Probleme, ne prend pas que les stations liés à un lotissement
-    @constraint(m, limit, sum(x[station] for station=1:nbStations) == p)
-    @objective(m, Max, dot(h,s))
-    optimize!(m)
-
-    if verbose
-        println("Nombre d'habitant déservi par cette solution : $(JuMP.objective_value(m))")
-        print("Les stations suivantes sont activées : ")
-        for iter = 1:length(x)
-            val = JuMP.value(x[iter])
-            if var != 0
-                print(stations[iter], " ")
-            end
-        end
-        println()
-    end
-
-    return m, x, s
-end
-
 function MCP_model_Lucas(stations::Array{Station,1},townsites::Array,p::Int, verbose::Bool = true)
     m = Model(GLPK.Optimizer)
     nbStations = length(stations)

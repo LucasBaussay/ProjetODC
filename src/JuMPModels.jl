ef610deee4997070ef18f810cb56b3f6414c7d79
jnicolasthouvenin <jules.nicolasthouvenin@gmail.com>
2020-11-22 12:43:56 +0100

    recuperation de la nuit blanche de Lucas

--- src/JuMPModels.jl
+++ src/JuMPModels.jl
@@ -3,7 +3,7 @@
 JuMPModels.jl gère les modèles JuMP et leur résolution
 """
 
-function MCP_model_Lucas(stations::Array{Station,1},townsites::Array,p::Int; verbose::Bool = true)
+function MCP_model_Lucas(stations::Array{Station,1},townsites::Array,p::Int, verbose::Bool = true)
     m = Model(GLPK.Optimizer)
     nbStations = length(stations)
     nbTownsites = length(townsites)
@@ -12,7 +12,7 @@ function MCP_model_Lucas(stations::Array{Station,1},townsites::Array,p::Int; ver
     @variable(m, x[stations], Bin) # xi = 1 si la station i est construite, 0 sinon
     @variable(m, s[townsites], Bin) # si = 1 si la demande du lotissement i est satisfaite, 0 sinon
 
-    @constraint(m, activation[townsite in townsites], sum(x[station]  for station in townsite.nearStations) - s[townsite] >= 0)
+    @constraint(m, activation[townsite in townsites], sum(x[station] - s[townsite] for station in townsite.nearStations) >= 0)
     @constraint(m, limit, sum(x[station] for station in stations) == p)
 
     @objective(m, Max, dot(h,s))
@@ -40,9 +40,8 @@ function MCP_model_Lucas(stations::Array{Station,1},townsites::Array,p::Int; ver
 end
 
 # PESP JuMP Model
-#=function PESP_model(T::Int,E::Array{Int,1},A_run::Array{Tuple{Int,Int},1},A_dwell::Array{Tuple{Int,Int},1},A_thr::Array{Tuple{Int,Int},1},A_head::Array{Tuple{Int,Int},1},A_reg::Array{Tuple{Int,Int},1},L::Array{Int,2},U::Array{Int,2})
-    arraysOfTuples = [A_run,A_dwell,A_head,A_thr,A_reg]
-    A = merge(arraysOfTuples) # fusionne les arrêtes
+function PESP_model(T::Int,E::Array{Node,1},A_run::Array{Tuple{Int,Int},1},A_dwell::Array{Tuple{Int,Int},1},A_thr::Array{Tuple{Int,Int},1},A_head::Array{Tuple{Int,Int},1},A_reg::Array{Tuple{Int,Int},1},L::Array{Int,2},U::Array{Int,2})
+    A = merge([A_run,A_dwell,A_head,A_thr,A_reg]) # fusionne les arrêtes
 
     nbNodes = length(E) # nombre de sommets
     nbArcs = length(A) # nombre d'arcs
@@ -51,14 +50,19 @@ end
     m = Model(GLPK.Optimizer)
 
     # déclaration des variables
-    @variable(m, 0 <= x[nbNodes] <= T) # temps d'arrivée à chaque sommet du graph
-    @variable(m, z[nbNodes,nbNodes], Bin) # modulo
+    @variable(m, 0 <= x[1:nbNodes] <= T) # temps d'arrivée à chaque sommet du graph
+    @variable(m, z[1:nbNodes,1:nbNodes], Bin) # modulo
 
     # déclaration des contraintes
     @constraint(m, valid[arc in A], L[arc[1],arc[2]] <= x[arc[2]] - x[arc[1]] + z[arc[1],arc[2]]*T <= U[arc[1],arc[2]])
 
     # déclaration de l'objectif
-    @objective(m, Min, )
+    @objective(m, Min, sum(x[arc[2]] - x[arc[1]] +z[arc[1], arc[2]] * T for arc in A_dwell) + sum(x[arc[2]] - x[arc[1]] +z[arc[1], arc[2]] * T for arc in A_run))
+
+    optimize!(m)
+
+    # Ce qu'il faut renvoyer : pour chaque Navette un Tableau pour chaque station (horaireDépart, horaireArrivé)
+    return m
 end
 
 # retourne le tableau de fusion résultante entre les cinq tableaux donnés de Tuple{Int,Int}
@@ -66,7 +70,7 @@ function merge(T::Array{Array{Tuple{Int,Int},1}})
     # calcul du nombre total de tuples
     nbTuples = 0
     for array in T
-        nbTuples += legnth(array)
+        nbTuples += length(array)
     end
     # allocation du vecteur à retourner
     result = Vector{Tuple{Int,Int}}(undef,nbTuples)
@@ -81,22 +85,124 @@ function merge(T::Array{Array{Tuple{Int,Int},1}})
     return result
 end
 
-# retourne un objet à passer au modèle JuMP PESP
-function parserPESP(T::Int,nbStations::Int,nbShuttles::Int,stations::Array{Station,1})
+# retourne un objet à passer au modèle JuMP PESP, avec nbStations > 0
+function parserPESP(T::Int,nbStations::Int,nbShuttles::Int,stations::Array{Station,1}, distStations::Array{Float64, 2})
+
+    dwellMax = 60 # en seconde
+    dwellMin = 30 # en seconde
+    vehicle_speed = 20 / 3.6 # en metre/seconde
+
     nbNodes = 4*(nbStations-1)*nbShuttles # nombre de sommets du graph
     nbNodesPerShuttle = 4*(nbStations-1)*nbShuttles # nombre de sommets du graph
-    nbArcs = 2*nbShuttles*(nbStation-1)*(nbShuttles+1) # nombre d'arcs
+    nbArcsRun = 2*nbShuttles*(nbStations-1) # nombre d'arcs run
+    nbArcsDwell = 2*nbShuttles*(nbStations-1) # nombre d'arcs Dwell
+    nbArcsReg = 2*nbShuttles*(nbStations-1)*(nbShuttles-1) # nombre d'arcs Reg
     # sommets du graph
     E = Array{Node,1}(undef,nbNodes)
     # arrêtes du graph
-    A = Array{Tuple{Int,Int},1}(undef,nbArcs)
+    A_run = Array{Tuple{Int,Int},1}(undef,nbArcsRun)
+    A_dwell = Array{Tuple{Int,Int},1}(undef,nbArcsDwell)
+    A_reg = Array{Tuple{Int,Int},1}(undef,nbArcsReg)
     # borne inférieure pour chaque couple de sommets du graph
     L = Array{Int,2}(undef,nbNodes,nbNodes)
     # borne supérieure pour chaque couple de sommets du graph
     U = Array{Int,2}(undef,nbNodes,nbNodes)
-    
-    # Calcul des sommets du graph
-end=#
+
+    iterEps = 0
+    iterARun = 0
+    iterADwell = 0
+    iterAReg = 0
+    for shuttle = 1:nbShuttles
+        # L'aller de la navette
+        iterEps += 1
+        E[iterEps] = Node(getNameNode(iterEps), shuttle, 1, departure, forth)
+        for iterStation = 2:nbStations-1
+            iterEps += 1
+            E[iterEps] = Node(getNameNode(iterEps), shuttle, iterStation, arrival, forth)
+
+            # Trajet entre une station et une autre
+            iterARun += 1
+            A_run[iterARun] = (iterEps-1, iterEps)
+            L[iterEps-1, iterEps] = Int(floor(0.95*distStations[ E[iterEps-1].indStation, E[iterEps].indStation] / vehicle_speed))
+            U[iterEps-1, iterEps] = Int(ceil(1.05*distStations[ E[iterEps-1].indStation, E[iterEps].indStation] / vehicle_speed))
+
+            iterEps += 1
+            E[iterEps] = Node(getNameNode(iterEps), shuttle, iterStation, departure, forth)
+
+            # Attente sur une station du trajet
+            iterADwell += 1
+            A_dwell[iterADwell] = (iterEps -1, iterEps)
+            L[iterEps-1, iterEps] = dwellMin
+            U[iterEps-1, iterEps] = dwellMax
+        end
+        iterEps += 1
+        E[iterEps] = Node(getNameNode(iterEps), shuttle, nbStations, arrival, forth)
+
+
+        iterARun += 1
+        A_run[iterARun] = (iterEps - 1, iterEps)
+        L[iterEps-1, iterEps] = Int(floor(0.95*distStations[ E[iterEps-1].indStation, E[iterEps].indStation] / vehicle_speed))
+        U[iterEps-1, iterEps] = Int(ceil(1.05*distStations[ E[iterEps-1].indStation, E[iterEps].indStation] / vehicle_speed))
+
+        # Le retour de la navette
+        iterEps += 1
+        E[iterEps] = Node(getNameNode(iterEps), shuttle, nbStations, departure, back)
+
+        iterADwell += 1
+        A_dwell[iterADwell] = (iterEps - 1, iterEps)
+        L[iterEps-1, iterEps] = dwellMin + 10
+        U[iterEps-1, iterEps] = dwellMax + 10
+
+        for iterStation = 1:nbStations-2
+            iterEps += 1
+            E[iterEps] = Node(getNameNode(iterEps), shuttle, nbStations - iterStation, arrival, back)
+
+            iterARun += 1
+            A_run[iterARun] = (iterEps - 1, iterEps)
+            L[iterEps-1, iterEps] = Int(floor(0.95*distStations[ E[iterEps-1].indStation, E[iterEps].indStation] / vehicle_speed))
+            U[iterEps-1, iterEps] = Int(ceil(1.05*distStations[ E[iterEps-1].indStation, E[iterEps].indStation] / vehicle_speed))
+
+            iterEps += 1
+            E[iterEps] = Node(getNameNode(iterEps), shuttle, nbStations - iterStation, departure, back)
+
+            iterADwell += 1
+            A_dwell[iterADwell] = (iterEps - 1, iterEps)
+            L[iterEps-1, iterEps] = dwellMin
+            U[iterEps-1, iterEps] = dwellMax
+        end
+        iterEps += 1
+        E[iterEps] = Node(getNameNode(iterEps), shuttle, 1, arrival, back)
+
+        iterARun += 1
+        A_run[iterARun] = (iterEps -1, iterEps)
+        L[iterEps-1, iterEps] = Int(floor(0.95*distStations[ E[iterEps-1].indStation, E[iterEps].indStation] / vehicle_speed))
+        U[iterEps-1, iterEps] = Int(ceil(1.05*distStations[ E[iterEps-1].indStation, E[iterEps].indStation] / vehicle_speed))
+
+        iterADwell += 1
+        A_dwell[iterADwell] = (iterEps, iterEps - 4*(nbStations - 1) +1)
+        L[iterEps , (iterEps - 4*(nbStations - 1) +1)] = dwellMin + 10
+        U[iterEps , (iterEps - 4*(nbStations - 1) +1)] = dwellMax + 10
+    end
+
+    for node = 1:(2*(nbStations-1))
+        for shuttle = 1:nbShuttles
+            for nextShuttle = (shuttle+1):nbShuttles
+                iterAReg += 1
+                A_reg[iterAReg] = (node + (shuttle - 1)*(4*(nbStations - 1)) , node + (nextShuttle - 1)*(4*(nbStations - 1)) )
+                L[node + (shuttle - 1)*(4*(nbStations - 1)), (node + (nextShuttle - 1)*(4*(nbStations - 1)))] = Int(floor(T/nbStations))
+                U[node + (shuttle - 1)*(4*(nbStations - 1)), (node + (nextShuttle - 1)*(4*(nbStations - 1)))] = typemax(Int)
+
+                iterAReg += 1
+                A_reg[iterAReg] = (node + (2*(nbStations-1)) + (shuttle - 1)*(4*(nbStations - 1)) , node + 2*(nbStations-1) + (nextShuttle - 1)*(4*(nbStations - 1)) )
+                L[node + (2*(nbStations-1)) + (shuttle - 1)*(4*(nbStations - 1)), (node + 2*(nbStations-1) + (nextShuttle - 1)*(4*(nbStations - 1)))] = Int(floor(T/nbStations))
+                U[node + (2*(nbStations-1)) + (shuttle - 1)*(4*(nbStations - 1)), (node + 2*(nbStations-1) + (nextShuttle - 1)*(4*(nbStations - 1)))] = typemax(Int)
+            end
+        end
+    end
+
+    return T, E, A_run, A_dwell, Vector{Tuple{Int, Int}}(), Vector{Tuple{Int, Int}}(), A_reg, L, U
+
+end
 
 function stationActivateGraph(listStations::Vector{Station}, listTownsites::Vector{Townsite})
     listP = collect(1:length(listStations))
@@ -113,3 +219,23 @@ function stationActivateGraph(listStations::Vector{Station}, listTownsites::Vect
     ylabel("Densité maximale couvrable")
     title("Nombre d'habitants touchés par nombre de stations ouverte sur la ligne")
 end
+
+# # retourne un objet à passer au modèle JuMP PESP
+# function parserPESP(T::Int,nbStations::Int,nbShuttles::Int,stations::Array{Station,1})
+#     nbNodes = 4*(nbStations-1)*nbShuttles # nombre de sommets du graph
+#     nbNodesPerShuttle = 4*(nbStations-1)*nbShuttles # nombre de sommets du graph
+#     nbArcs = 2*nbShuttles*(nbStation-1)*(nbShuttles+1) # nombre d'arcs
+#     # sommets du graph
+#     E = Array{Node,1}(undef,nbNodes)
+#     # arrêtes du graph
+#     A = Array{Tuple{Int,Int},1}(undef,nbArcs)
+#     # borne inférieure pour chaque couple de sommets du graph
+#     L = Array{Int,2}(undef,nbNodes,nbNodes)
+#     # borne supérieure pour chaque couple de sommets du graph
+#     U = Array{Int,2}(undef,nbNodes,nbNodes)
+#
+#     # Calcul des sommets du graph
+#     for indexNode in 1:nbNodes/nbShuttles
+#         E[indexNode] = Node(getNameNode(indexNode),)
+#     end
+# end
--- src/main.jl
+++ src/main.jl
@@ -5,7 +5,11 @@ main.jl est le fichier principal.
 
 println("Precompiling packages...")
 
+
 using JuMP, GLPK, LinearAlgebra, PyPlot
+
+@enum Sense forth back
+@enum State departure arrival
 
 include("structs.jl")
 include("dataManager.jl")
@@ -24,22 +28,7 @@ const SPEED_VEHICLE = 20
 const STOP_TIME_STATION = 3
 
 
-
-function main()
-    stations = getStations("stations.dat")
-    townsites = getTownsites("townsites.dat",stations)
-    h = getDensities(townsites)
-    Z = Vector{Int}(undef,length(stations))
-    for p in 1:length(stations)
-        x,z = MCP_model_Lucas(stations,townsites,p,verbose=false)
-        Z[p] = z
-        println("x = ",x)
-    end
-    plot(Z)
-    return Z
-end
-
-function jules(p::Int)
+function jules()
     stations = getStations("stations.dat")
     println(stations)
     townsites = getTownsites("townsites.dat",stations)
@@ -47,7 +36,7 @@ function jules(p::Int)
 
     h = getDensities(townsites)
     println("h = ",h)
-    x,z = MCP_model_Lucas(stations,townsites,p,verbose=true)
+    x,z = MCP_model_Lucas(stations,townsites,10,true)
     println("z = ",z)
     println("x = ",x)
 end
@@ -58,3 +47,61 @@ function lucas()
 
     return stations, townsites
 end
+
+function testDidactic()
+    T = 1 * 60 * 60
+    nbStations = 3
+    nbShuttles = 1
+
+    stations = [Station("Station "*string(i), rand(), rand()) for i = 1:nbStations]
+    distStations = Array{Float64, 2}(undef, nbStations, nbStations)
+
+    distStations[1,2] = 750
+    distStations[2,3] = 1000
+    distStations[2,1] = 750
+    distStations[3,2] = 1000
+
+    T, E, A_run, A_dwell, A_thr, A_head, A_reg, L, U = parserPESP(T, nbStations, nbShuttles, stations, distStations)
+    m = PESP_model(T, E, A_run, A_dwell, A_thr, A_head, A_reg, L, U)
+
+    return m
+end
+
+"""
+Pour Jules :
+
+La fonction PESP_model te renvoie le modèle JuMP oprimisé.
+
+Fonctions utiles dessus :
+
+Récuperer le modèle :
+
+m = testDidactic()
+
+Les contraintes liées aux variables
+
+- JuMP.all_constraints(m, JuMP.VariableRef, MOI.GreaterThan{Float64})
+- JuMP.all_constraints(m, JuMP.VariableRef, MOI.LessThan{Float64})
+- JuMP.all_constraints(m, JuMP.VariableRef, MOI.ZeroOne)
+
+Les réels contraintes
+
+- JuMP.all_constraints(m, JuMP.GenericAffExpr{Float64, VariableRef}, MOI.Interval{Float64})
+
+La fonction objectif
+
+- JuMP.objective_function(m)
+
+Le numéro d'indicage des contraintes est le numéro du sommet associé, l'ordre de construction est montré (si c'est lisible) sur le graphique que je t'ai envoyé sur Messenger.
+
+Cordialement
+
+Le problème vient des terminus sur le retour : En effet, quand on verifie les temps π_j - π_i, pour le terminus du retour on vient à faire :
+
+π_{premier de la navette} - π_{dernier de la navette} :
+
+Dans les faits on calcul : 40 <= 0 - 820 + z * 3600 <= 70
+
+Tu peux prendre z = 1 ou 0 ca ne satisfait pas la contrainte donc le problème est infaisable ! Je crois qu'on a mal compris une partie du sujet
+Donc si tu trouves l'erreur je t'appel maitre ! (Car l'erreur ne vient pas du code parce qu'il fait exactement tout ce que je veux mais plutôt de la comprehension du problème.)
+"""

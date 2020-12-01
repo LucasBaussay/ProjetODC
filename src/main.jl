28c769cc35d66f44de6ae976b4888c14c3f0c0ee
Lucas Baussay <lucas.baussay.lb@gmail.com>
2020-12-01 11:31:39 +0100

    Ajout du nouveau graph

--- src/main.jl
+++ src/main.jl
@@ -27,17 +27,61 @@ const SPEED_VEHICLE = 20
 
 const STOP_TIME_STATION = 3
 
+
+function formul(lat1::Float64, long1::Float64, lat2::Float64, long2::Float64)
+    r = 6371008
+    distAng = acos( sin(lat1 * pi/180) * sin(lat2 * pi/180) + cos(lat1 * pi/180) * cos(lat2 * pi/180) * cos((long1 - long2)*pi/180) )
+
+    return r * distAng
+end
+
+function calculDistLatLong(stations::Vector{Station})
+
+    nbStations = length(stations)
+    distStations = Array{Float64, 2}(undef, nbStations, nbStations)
+
+    for iter = 1:(nbStations-1)
+		dist = formul(stations[iter].latitude, stations[iter].longitude, stations[iter+1].latitude, stations[iter+1].longitude)
+        distStations[iter, iter+1] = dist
+		distStations[iter+1, iter] = dist
+    end
+
+	for iterRow = 1:nbStations
+		for iterCol = (iterRow+2):nbStations
+			distStations[iterRow, iterCol] = 0.
+			distStations[iterCol, iterRow] = 0.
+			for iter = 1:(iterCol-iterRow)
+				distStations[iterRow, iterCol] += distStations[iter, iter+1]
+				distStations[iterCol, iterRow] += distStations[iter, iter+1]
+			end
+		end
+	end
+    return distStations
+end
+
 function mainPart1()
     stations = getStations("stations.dat")
+    distStations = calculDistLatLong(stations)
     townsites = getTownsites("townsites.dat",stations)
     h = getDensities(townsites)
     Z = Vector{Int}(undef,length(stations))
+    couts = zeros(Float64,length(stations))
     for p in 1:length(stations)
         x,z = MCP_model_Lucas(stations,townsites,p,false)
-        #println("z = ",z)
+        
         Z[p] = z
+        
+        couts[p] = 1.9 * p
+        if x[end].ind >= 10
+        	couts[p] += 5.9 * distStations[10, x[end].ind]/1000
+        end
+        
     end
-    plot(Z,marker=".",label="Nombre d'habitants qui ont au moins une station accessible")
+    plot(Z, couts, marker=".",label="Nombre d'habitants qui ont au moins une station accessible")
+    grid()
+    title("Représentation pour chaque nombre de station ouverte, le cout en fonction de la densité de population touchée")
+    xlabel("Densité de population")
+    ylabel("Cout d'installation en Millions d'€")
     legend()
 end
 
@@ -68,37 +112,6 @@ function distDidact()
     distStations[2,3] = 1000
     distStations[2,1] = 750
     distStations[3,2] = 1000
-    return distStations
-end
-
-function formul(lat1::Float64, long1::Float64, lat2::Float64, long2::Float64)
-    r = 6371008
-    distAng = acos( sin(lat1 * pi/180) * sin(lat2 * pi/180) + cos(lat1 * pi/180) * cos(lat2 * pi/180) * cos((long1 - long2)*pi/180) )
-
-    return r * distAng
-end
-
-function calculDistLatLong(stations::Vector{Station})
-
-    nbStations = length(stations)
-    distStations = Array{Float64, 2}(undef, nbStations, nbStations)
-
-    for iter = 1:(nbStations-1)
-		dist = formul(stations[iter].latitude, stations[iter].longitude, stations[iter+1].latitude, stations[iter+1].longitude)
-        distStations[iter, iter+1] = dist
-		distStations[iter+1, iter] = dist
-    end
-
-	for iterRow = 1:nbStations
-		for iterCol = (iterRow+2):nbStations
-			distStations[iterRow, iterCol] = 0.
-			distStations[iterCol, iterRow] = 0.
-			for iter = 1:(iterCol-iterRow)
-				distStations[iterRow, iterCol] += distStations[iter, iter+1]
-				distStations[iterCol, iterRow] += distStations[iter, iter+1]
-			end
-		end
-	end
     return distStations
 end
 
